6bc1752d68d37fb98bfbb223b382cd4526ba00ee
ZwingliCaleb <calebzwingli@gmail.com>
2023-02-06 09:03:06 -0800

    Base Geometry class has a public instance method that raises an exception with a message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""contains area function"""
+
+class BaseGeometry:
+    """class with a public attribute are"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
