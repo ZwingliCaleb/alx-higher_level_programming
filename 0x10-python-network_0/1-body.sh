dbcdb4320251af6fdcda3ce0ed5a0072bca4a733
ZwingliCaleb <calebzwingli@gmail.com>
2023-04-27 13:22:29 -0700

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-# Bash script taking url sends GET request to the URL and displays body of the response
-
-if [ $# -eq 0 ]; then
-  echo "Error: URL argument not provided"
-  exit 1
-fi
-
-response=$(curl -s -X GET $1)
-if [ $(echo "$response" | grep -c 'HTTP/1\.[01] 200') -eq 0 ]; then
-  echo "Error: Could not retrieve URL or status code was not 200"
-  exit 1
-fi
-
-echo "$response"
-
+# bash scriptthat takes url sends a get request to the url and displays body of response
+curl -s $1 | (read status; [ ${status:9:3} -eq 200 ] && cat)
