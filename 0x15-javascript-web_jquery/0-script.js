7a018bec97e2ebf7ee4e9e0754c6bd7fe8ebbe4e
ZwingliCaleb <calebzwingli@gmail.com>
2023-05-31 12:37:17 -0700

    change/edit text color

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+//Add js script that updates text color
+
+document.addEventListener('DOMContentLoaded', function() {
+  var header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
