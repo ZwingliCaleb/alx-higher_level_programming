a2841eb0ed949033588d0238a7f544ff7126ecb4
ZwingliCaleb <calebzwingli@gmail.com>
2023-05-31 13:45:49 -0700

    udate header element to red

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// This js script updates the text color of the header element to red
+
+document.addEventListener('DOMContentLoaded', function() {
+  var header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
